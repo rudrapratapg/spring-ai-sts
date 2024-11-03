3c58a08a48facde36c235235a0b19dbe9322e40e
Deepak Gola <rudra.pratap91@live.com>
2024-11-03 22:54:44 +0530

    Revert "Feat/003 prompt enginnering"

--- src/main/java/com/projectai/springai/model/GetCapitalResponse.java
+++
@@ -1,7 +0,0 @@
-package com.projectai.springai.model;
-
-import com.fasterxml.jackson.annotation.JsonPropertyDescription;
-
-public record GetCapitalResponse(@JsonPropertyDescription("This is the city name") String answer) {
-
-}
--- src/main/java/com/projectai/springai/service/OpenAIService.java
+++ src/main/java/com/projectai/springai/service/OpenAIService.java
@@ -1,23 +1,9 @@
 package com.projectai.springai.service;
 
 import com.projectai.springai.model.Answer;
-import com.projectai.springai.model.GetCapitalInfoResponse;
-import com.projectai.springai.model.GetCapitalRequest;
-import com.projectai.springai.model.GetCapitalResponse;
-import com.projectai.springai.model.Question;
 
 public interface OpenAIService {
 	
-	Answer getAnswer(Question question);
-	
-	Answer getCapital(GetCapitalRequest getCapitalRequest);
-	
-	GetCapitalResponse getCapitalSchema(GetCapitalRequest getCapitalRequest);
-	
-	Answer getCapitalWithInfo(GetCapitalRequest getcapitalRequest);
-	
-	GetCapitalInfoResponse getCapitalWithInfoSchema(GetCapitalRequest getcapitalRequest);
+	Answer getAnswer(String question);
 
-	Answer getCapitalWithInfoJson(GetCapitalRequest getCapitalRequest);
-	
 }
